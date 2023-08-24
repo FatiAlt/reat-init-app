7862c37aa6d853f71478b94d868514e50cb836f1
Fatima Kaaouan <fatima.kaaouan@octo.com>
2023-08-24 13:33:52 +0200

    add radio

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -5,6 +5,8 @@ import Card from './Card';
 const Countries = () => {
     const [data, setData] = useState([]); //Le useState permet de changer la valeur de la variable
     const [rangeValue, setRangeValue] = useState(36); // le chiffre 36 est un multiple de 12, qui est souvent utilisé pour afficher des cartes
+    const [selectedRadio, setSelectedRadio] = useState("");
+    const radios = ["Africa", "America", "Asia", "Europe", "Océania"];
     //Le useEffect se joue lorsque le composant est monté
     useEffect(() => {
         axios.get("https://restcountries.com/v3.1/all").then((res) => {
@@ -15,18 +17,35 @@ const Countries = () => {
     return (
         <div className='countries'>
             <ul className='radio-container'>
-                <input type="range"
+                <input
+                type="range"
                  min="1"
                  max="250"
                  defaultValue={rangeValue}
                  onChange={(e) =>
                 setRangeValue(e.target.value)} // onChange est un évèvement qui permet de modifier la valeur rangeValue
                 />
+                {radios.map((continent) => (
+                    <li>
+                      <input
+                      type="radio"
+                      id={continent}
+                      name="continentRadio"
+                      checked={continent === selectedRadio}
+                      onChange={(e) => setSelectedRadio(e.target.id)}/>
+                      <label htmlFor={continent}>{continent}</label>
+                      </li>
+                ))}
+
 
             </ul>
-            <h1>Countries</h1>
+            {selectedRadio &&
+            <button onClick={() => setSelectedRadio("")}>Annuler la recherche</button> }
+            {/* <h1>Countries</h1> */}
             <ul>
                 {data
+                .filter((country) => country.continents[0].includes(selectedRadio)) //d'abord filtrer, ensuite trier et couper le tableau
+                .sort((a,b) => b.population - a.population)
                 .slice(0, rangeValue)
                 .map((country, index) =>
                  //<li key={index}>{country.translations.fra.commons}</li>
